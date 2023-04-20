d0a24085ea4acdf3f52c97217b29768c79227dec
AlexAzyavchikov <azyavchikov.alex@gmail.com>
2023-04-20 22:38:28 +0300

    database "bsu_cool_chat_db" connected

---
+++ BSU_cool_chat/src/main/java/project/dao/UserMapper.java
@@ -0,0 +1,20 @@
+package project.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import project.models.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper implements RowMapper<User> {
+    @Override
+    public User mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
+        User user = new User();
+
+        user.setId(resultSet.getInt("id"));
+        user.setLogin(resultSet.getString("login"));
+        user.setPassword(resultSet.getString("password"));
+
+        return user;
+    }
+}
