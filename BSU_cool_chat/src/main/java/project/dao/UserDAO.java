6480dd6cd4a926767036dd705f535b9c4a7bcde9
AzyavchikovAlex <99528478+AzyavchikovAlex@users.noreply.github.com>
2023-05-01 10:15:31 +0300

    All required components are created (#20)

--- BSU_cool_chat/src/main/java/project/dao/UserDAO.java
+++ BSU_cool_chat/src/main/java/project/dao/UserDAO.java
@@ -5,11 +5,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import project.models.User;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Optional;
 
 @Component
-public class UserDAO {
+public class UserDAO implements UserService {
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
@@ -17,26 +18,27 @@ public class UserDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<User> getUsers() {
+    @PostConstruct
+    void postConstruct() {
+        jdbcTemplate.update("CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, login varchar, password varchar)");
+    }
+
+    public List<User> getAllUsers() {
         return jdbcTemplate.query("SELECT * FROM users", new UserMapper());
 //        return jdbcTemplate.query("SELECT * FROM users", new BeanPropertyRowMapper<>(User.class));
     }
 
-    public User getUser(int id) {
-        return jdbcTemplate.query("SELECT * FROM users WHERE id=?", new Object[]{id}, new UserMapper())
-                .stream()
-                .findAny()
-                .orElse(null);
-//                .orElse(new Error(id + "not found"));
-    }
-
-    public void addUser(User user) {
+    public void createUser(User user) {
         if (jdbcTemplate.query("SELECT * FROM users WHERE login=?",
                 new Object[]{user.getLogin()},
                 new UserMapper()).stream().findAny().isPresent()) {
             throw new RuntimeException("user with login \"" + user.getLogin() + "\" already exists");
         }
-        jdbcTemplate.update("INSERT INTO users(login, password) VALUES (?, ?);",user.getLogin(), user.getPassword());
+        jdbcTemplate.update("INSERT INTO users(login, password) VALUES (?, ?);", user.getLogin(), user.getPassword());
+    }
+
+    public void deleteUser(int id) {
+        throw new RuntimeException("no such method");
     }
 
     public void updateUser(User user) {
@@ -46,8 +48,16 @@ public class UserDAO {
                 user.getId());
     }
 
+    public User getById(int id) {
+        return jdbcTemplate.query("SELECT * FROM users WHERE id=?", new Object[]{id}, new UserMapper())
+                .stream()
+                .findAny()
+                .orElse(null);
+//                .orElse(new Error(id + "not found"));
+    }
+
     public Optional<Integer> getUserId(String login, String password) {
-        var searching_user = getUsers().stream()
+        var searching_user = getAllUsers().stream()
                 .filter(user -> user.getLogin().equals(login) && user.getPassword().equals(password))
                 .findAny();
         return searching_user.map(user -> Math.toIntExact(user.getId()));
