684ae91aaecbb22968b163a978faa7a971955c58
AzyavchikovAlex <99528478+AzyavchikovAlex@users.noreply.github.com>
2023-05-18 13:13:29 +0300

    search added (#37)

--- BSU_cool_chat/src/main/java/project/Controllers/UsersController.java
+++ BSU_cool_chat/src/main/java/project/Controllers/UsersController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.*;
 import project.dao.User.UserService;
 import project.models.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/users")
 public class UsersController {
@@ -39,5 +42,23 @@ public class UsersController {
     public String create(@ModelAttribute("user") User user) {
         userService.createUser(user);
         return "redirect:/users";
+    }
+
+    @GetMapping("/search/{user_id}/init_page")
+    public String search(@PathVariable("user_id") int user_id,
+                         Model model) {
+        model.addAttribute("user_id", user_id);
+        model.addAttribute("searching_user", new User());
+        return "users/search";
+    }
+
+    @GetMapping("/search/{user_id}")
+    public String search(@PathVariable("user_id") int user_id,
+                         @ModelAttribute("searching_user") User searchingUser,
+                         Model model) {
+        model.addAttribute("user_id", user_id);
+        model.addAttribute("searching_user", new User());
+        model.addAttribute("found_users", userService.getAllSimilarUsers(searchingUser.getLogin()));
+        return "users/search";
     }
 }
