9eca9495c469c3b33e8fe3aecd1548421ad7d25e
AzyavchikovAlex <99528478+AzyavchikovAlex@users.noreply.github.com>
2023-05-21 22:39:35 +0300

    Login and password validation (#46)

--- BSU_cool_chat/src/main/java/project/models/User.java
+++ BSU_cool_chat/src/main/java/project/models/User.java
@@ -1,9 +1,16 @@
 package project.models;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class User {
     private int id;
+    @NotEmpty(message = "Login should not be empty")
+    @Size(min = 2, max = 30, message = "Login should be between 2 and 30 characters")
     private String login;
 
+    @NotEmpty(message = "Password should not be empty")
+    @Size(min = 2, max = 30, message = "Password should be between 2 and 30 characters")
     private String password;
 
     public User() {
