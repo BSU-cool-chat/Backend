5a1eb920adc4d50cd7f5a834b629ba16bd869326
Adamenko-Vladislav <vladadam20@gmail.com>
2023-05-28 08:35:41 +0300

    logs

--- BSU_cool_chat/src/main/java/project/Controllers/RegisterController.java
+++ BSU_cool_chat/src/main/java/project/Controllers/RegisterController.java
@@ -24,7 +24,7 @@ public class RegisterController {
     public String GreetingPage(Model model) {
         model.addAttribute("user", new User());
         model.addAttribute("is_login_mistake_occur", false);
-        SingletonLogger.getInstance().config("GreetingPage");
+        SingletonLogger.getInstance().info("GreetingPage");
         return "registration/login_page";
     }
 
@@ -32,13 +32,14 @@ public class RegisterController {
     public String LogIn(@ModelAttribute("user") User user,
                         Model model) {
         var id = userService.getUserId(user.getLogin(), user.getPassword());
-        SingletonLogger.getInstance().config("LogIn user_id: " + String.valueOf(user.getId()));
         if (id.isEmpty()) {
             user.setPassword(null);
             model.addAttribute("user", user);
             model.addAttribute("is_login_mistake_occur", true);
+            SingletonLogger.getInstance().info("user_id: " + String.valueOf(user.getId()));
             return "registration/login_page";
         }
+        SingletonLogger.getInstance().info("user_id: " + String.valueOf(user.getId()));
         return "redirect:/chats/" + id.get();
     }
 }
